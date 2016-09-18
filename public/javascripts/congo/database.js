d51fda4eed5f3fd954d625bb1c81b14d2ced3c7b
mangel0111 <m.angel011192@gmail.com>
2016-09-18 11:47:45 -0300

    Add the model save options

--- public/javascripts/congo/database.js
+++ public/javascripts/congo/database.js
@@ -1,5 +1,8 @@
 Congo.Database = Backbone.Model.extend({
-
+    url: function () { 
+        return "/mongo-api/dbs/"+this.id;
+    },
+    idAttribute: "name"
 });
 
 Congo.DatabaseCollection = Backbone.Collection.extend({
@@ -29,9 +32,13 @@ Congo.DatabaseListView = Backbone.View.extend({
         this.collection.bind("reset", this.render, this);
         this.collection.bind("add", this.render, this);
         this.collection.bind("remove", this.render, this);
+        this.renderOptionView();
     },
     tagName: "table",
     className: "table table-striped",
+    renderOptionView: function () {
+        var optionView = new Congo.DatabaseOptionView({ el: "#db-options" });
+    },
     render: function () {
         var dbList = [];
         this.collection.each(function (item) {
@@ -42,4 +49,26 @@ Congo.DatabaseListView = Backbone.View.extend({
         // return this;
         $('#database-list').html(this.el);
     }
+});
+
+
+Congo.DatabaseOptionView = Backbone.View.extend({
+    initialize: function () {
+        this.render();
+    },
+    events: {
+        "submit form": "addDb"
+    },
+    addDb: function (e) {
+        e.preventDefault();
+        var newDb = new Congo.Database({ name: $('#newDb').val() });
+        console.log(newDb);
+    },
+    render: function () {
+        var source = $('#new-db-template').html();
+        var compiled = _.template(source);
+        this.$el.html(compiled);
+        return this;
+    }
+
 });
